596cb22b9f5d21da37b34ba6eb57f346741bccc8
Josemar666 <josemarlob1@hotmail.com>
2021-03-05 19:47:21 -0300

    depois de apagar 2 commit foram feita alterações

--- src/vouvencer/VouVencer.java
+++ src/vouvencer/VouVencer.java
@@ -7,6 +7,12 @@ public class VouVencer {
     public static void main(String[] args) {
         System.out.println("Vou aprender usar o github !!!");
         System.out.println("vou praticar muito até aprender a usar o github");
+        System.out.println("nova alteração no projeto");
+        int a = 2;
+        int b = 3;
+        int soma = a+b;
+        System.out.println(soma);
+        System.out.println("alterando pela  5 vez");
     }
     
 }
